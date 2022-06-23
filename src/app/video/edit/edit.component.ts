e81dcd1d33063ff55dae6b7e3639f101a4f6c3ec
Alex SIEYADJEU <alex.sieyadjeu@externe.maif.fr>
2022-06-23 14:30:34 +0200

    upload, view and edit video

---
+++ src/app/video/edit/edit.component.ts
@@ -0,0 +1,44 @@
+import {Component, OnInit, OnDestroy, Input, OnChanges, SimpleChanges} from '@angular/core';
+import {ModalService} from "../../services/modal.service";
+import IClip from "../../models/clip.model";
+import {FormControl, FormGroup, Validators} from "@angular/forms";
+
+@Component({
+  selector: 'app-edit',
+  templateUrl: './edit.component.html',
+  styleUrls: ['./edit.component.css']
+})
+export class EditComponent implements OnInit, OnDestroy, OnChanges {
+  @Input() activeClip: IClip | null = null
+
+  clipID = new FormControl('')
+  title = new FormControl('', [
+    Validators.required,
+    Validators.minLength(3)
+  ])
+  editForm = new FormGroup({
+    title: this.title,
+    id: this.clipID
+  })
+
+  constructor(private modal: ModalService) {
+  }
+
+  ngOnInit(): void {
+    this.modal.register('editClip')
+  }
+
+  ngOnChanges(): void {
+    if (!this.activeClip) {
+      return
+    }
+
+    this.clipID.setValue(this.activeClip.docID)
+    this.title.setValue(this.activeClip.title)
+  }
+
+  ngOnDestroy() {
+    this.modal.unregister('editClip')
+  }
+
+}
